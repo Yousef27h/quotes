e9b589b0f373ae18c6a85189dd12a2a91257739a
Yousef27h <yousef_salem97@yahoo.com>
2021-07-06 23:28:48 +0300

    parsing JSON file

---
+++ app/src/main/java/quotes/Quotes.java
@@ -0,0 +1,17 @@
+package quotes;
+
+public class Quotes {
+    String[] tags;
+    String author;
+    String likes;
+    String text;
+
+    public Quotes(String[] tags, String author, String likes, String text) {
+        this.tags = tags;
+        this.author = author;
+        this.likes = likes;
+        this.text = text;
+    }
+
+
+}
